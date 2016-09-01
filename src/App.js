50736b464f28649be825da6b591c67e622d92363
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-01 16:39:50 +0800

    add damageMatrix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-
 import './App.css';
-
-// eslint-disable-next-line
-const types = [
-  'normal',
-  'fighting',
-  'flying',
-  'poison',
-  'ground',
-  'rock',
-  'bug',
-  'ghost',
-  'steel',
-  'fire',
-  'water',
-  'grass',
-  'electric',
-  'psychic',
-  'ice',
-  'dragon',
-  'dark',
-  'fairy'
-];
 
 class App extends Component {
   render() {
