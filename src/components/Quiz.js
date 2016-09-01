d185c3d905bcb6b3ac6b916cb2960b1fc979109c
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-01 15:00:44 +0800

    add data format for quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,29 +2,63 @@ import React, { Component } from 'react';
 import './Quiz.css';
 
 class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 60,
+      combo: 0,
+      status: '',
+      quiz: {
+        emeny: {
+          title: 'attack emeny',
+          type: 'dragon'
+        },
+        options: [
+          {
+            type: 'normal',
+            value: 1
+          },
+          {
+            type: 'poison',
+            value: 1
+          },
+          {
+            type: 'fairy',
+            value: 2
+          },
+          {
+            type: 'electric',
+            value: 0.5
+          }
+        ],
+        answer: 2
+      }
+    }
+  }
+
   render() {
+    const { quiz, combo, status } = this.state;
+    const { emeny, options } = quiz;
+
     return (
       <div className="Quiz">
         <div className="stemContainer Ta(c) C(white) Bgc-dragon">
-          <div className="stemCombo">combo: 18</div>
+          <div className="stemCombo">combo: {combo}</div>
           <div className="stemEnemy">
-            <div className="stemEnemyText">attack enemy<br/>( dragon )</div>
+            <div className="stemEnemyText">{emeny.title}<br/>( {emeny.type} )</div>
           </div>
-          <div className="stemStatus">super effective</div>
+          <div className="stemStatus">{status}</div>
         </div>
         <div className="optionContainer">
-          <div className="optionItem Ta(c) W(50%) C(white) Bgc-normal">
-            <div className="optionText">normal</div>
-          </div>
-          <div className="optionItem Ta(c) W(50%) C(white) Bgc-poison">
-            <div className="optionText">poison</div>
-          </div>
-          <div className="optionItem Ta(c) W(50%) C(white) Bgc-fairy">
-            <div className="optionText">fairy</div>
-          </div>
-          <div className="optionItem Ta(c) W(50%) C(white) Bgc-electric">
-            <div className="optionText">electric</div>
-          </div>
+          {options.map((option, index) => {
+            const { type } = option;
+            return (
+              <div className={"optionItem Ta(c) W(50%) C(white) Bgc-" + type}
+                key={type + index}>
+                <div className="optionText">{type}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
