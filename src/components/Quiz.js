e8f41d1aa0ab09089bee596ddf185aec22ee97b1
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-01 15:08:42 +0800

    add timer and countdown behavior

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,48 +2,68 @@ import React, { Component } from 'react';
 import './Quiz.css';
 
 class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timer: 60,
-      combo: 0,
-      status: '',
-      quiz: {
-        emeny: {
-          title: 'attack emeny',
-          type: 'dragon'
+  static contextTypes = {
+    router: React.PropTypes.object
+  }
+
+  state = {
+    timer: 5,
+    combo: 0,
+    status: 'super effective',
+    quiz: {
+      emeny: {
+        title: 'attack emeny',
+        type: 'dragon'
+      },
+      options: [
+        {
+          type: 'normal',
+          value: 1
         },
-        options: [
-          {
-            type: 'normal',
-            value: 1
-          },
-          {
-            type: 'poison',
-            value: 1
-          },
-          {
-            type: 'fairy',
-            value: 2
-          },
-          {
-            type: 'electric',
-            value: 0.5
-          }
-        ],
-        answer: 2
-      }
+        {
+          type: 'poison',
+          value: 1
+        },
+        {
+          type: 'fairy',
+          value: 2
+        },
+        {
+          type: 'electric',
+          value: 0.5
+        }
+      ],
+      answer: 2
     }
   }
 
+  componentDidMount() {
+    this.timerInterval = setInterval(() => {
+      this.setState({
+        timer: this.state.timer - 1
+      }, () => {
+        if (this.state.timer === 0) {
+          clearInterval(this.timerInterval);
+        }
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerInterval);
+  }
+
   render() {
-    const { quiz, combo, status } = this.state;
+    const { timer, combo, status, quiz } = this.state;
     const { emeny, options } = quiz;
 
     return (
       <div className="Quiz">
         <div className="stemContainer Ta(c) C(white) Bgc-dragon">
-          <div className="stemCombo">combo: {combo}</div>
+          <div className="stemHeader">
+            <div className="stemTimer">timer: {timer}</div>
+            <div className="stemCombo">combo: {combo}</div>
+          </div>
           <div className="stemEnemy">
             <div className="stemEnemyText">{emeny.title}<br/>( {emeny.type} )</div>
           </div>
