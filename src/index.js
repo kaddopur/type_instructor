4eaf18878ff7983c82454ed1c952a1d05ef0d22f
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-08 23:25:38 +0800

    integrate redux

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ import Lang from './components/Lang';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  }),
+  window.devToolsExtension ? window.devToolsExtension() : undefined
+);
+
+const history = syncHistoryWithStore(hashHistory, store);
+
+
 class App extends Component {
   static childContextTypes = {
     messages: PropTypes.object
@@ -30,14 +44,16 @@ class App extends Component {
 
 ReactDOM.render(
   <App>
-    <Router history={hashHistory}>
-      <Route path="/" component={Home} />
-      <Route path="/:lang/" component={Home} />
-      <Route path="/:lang/menu" component={Menu} />
-      <Route path="/:lang/lang" component={Lang} />
-      <Route path="/:lang/quizzes/:catetory/:gameType/:quizType" component={Quiz} />
-      <Route path="/:lang/quizzes/:catetory/:gameType/:quizType/result/:value" component={Result} />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={Home} />
+        <Route path="/:lang/" component={Home} />
+        <Route path="/:lang/menu" component={Menu} />
+        <Route path="/:lang/lang" component={Lang} />
+        <Route path="/:lang/quizzes/:catetory/:gameType/:quizType" component={Quiz} />
+        <Route path="/:lang/quizzes/:catetory/:gameType/:quizType/result/:value" component={Result} />
+      </Router>
+    </Provider>
   </App>,
   document.getElementById('root')
 );
